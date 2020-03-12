0d098318d1a772c5a3a8eab0f6895f9b38c9361e
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-03-11 22:46:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Version25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print 1-10 numbers onto the console 
+            int i; // To count
+            int start = 1;
+            int end = 10;
+
+            int stringnumber;
+            Console.WriteLine("Hello, my name is Mikayla Anne Mendez and this is Version=25!");
+            Console.Write("Enter any string of characters:  ");
+
+            // Ask User to enter a value between two and five
+            Console.WriteLine("Enter an Integer between 2 and 5:  ");
+            stringnumber = Convert.ToInt32(Console.ReadLine());
+            
+            if ()
+
+
+        }
+    }
+}
