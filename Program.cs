b1638e7c9bc0895a466f776c0c3d8f6bdabc1055
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-03-11 23:46:30 -0400

    Version 2

--- Program.cs
+++ Program.cs
@@ -7,21 +7,72 @@ namespace Version25
         static void Main(string[] args)
         {
             // Print 1-10 numbers onto the console 
-            int i; // To count
+            int i;
             int start = 1;
             int end = 10;
+            
+
 
             int stringnumber;
             Console.WriteLine("Hello, my name is Mikayla Anne Mendez and this is Version=25!");
+
+            string characters;
             Console.Write("Enter any string of characters:  ");
+            characters = Console.ReadLine();
 
             // Ask User to enter a value between two and five
-            Console.WriteLine("Enter an Integer between 2 and 5:  ");
+            Console.WriteLine("Enter an number between 2 and 5:  ");
             stringnumber = Convert.ToInt32(Console.ReadLine());
-            
-            if ()
 
+
+
+            string single;
+            // Ask User to enter a single character
+            Console.Write("Enter a single character:  ");
+            single = Console.ReadLine();
+
+
+            if (stringnumber == 2)
+            {
+                for (i = start; i <= stringnumber; i++)
+                {
+                    Console.Write(characters);
+                    Console.Write(single);
+
+                }
+            }
+            else if (stringnumber == 3)
+            {
+                for (i = start; i <= stringnumber; i++)
+                {
+                    Console.Write(characters);
+                    Console.Write(single);
+
+                }
+
+            }
+            else if (stringnumber == 4)
+            {
+                for (i = start; i <= stringnumber; i++)
+                {
+                    Console.Write(characters);
+                    Console.Write(single);
+
+                }
+            }
+            else if (stringnumber == 5)
+            {
+                for (i = start; i <= stringnumber; i++)
+                {
+                    Console.Write(characters);
+                    Console.Write(single);
+
+                }
+            }
+
+           
 
         }
+
     }
 }
